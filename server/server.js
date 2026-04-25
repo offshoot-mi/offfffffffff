10ee7c3896d1203f71fb5526107caf62a864c6e4
offshoot-mi <offshoot019@gmail.com>
2026-04-25 12:40:25 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -67,7 +67,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, '../../client/dist', 'index.html'));
   });
 }
-=======
+
 // ------------------- Static Frontend -------------------
 const staticPath = path.join(__dirname, '..', 'client', 'dist');
 
